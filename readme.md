470f14c9e5539958c7b8accb7ca0b9ddd3da9aa0
Aashutosh Soni <ashutoshsoni130@gmail.com>
2023-03-05 13:23:59 +0530

    Completed

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Step by Step form
+ a 3 phase form to simply use in any project.
+ 
+# Contribute?
+improve this form by improving UI, adding new features etc, simply create a fork, work and make a PR.
+
+Have a Good Day!✨
---
+++ script.js
@@ -0,0 +1,123 @@
+console.log("Script.js loading")
+
+//loading standard page components
+const previousButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+const submitButton = document.querySelector('#submit')
+const tabTargets = document.querySelectorAll('.tab')
+const tabPanels = document.querySelectorAll('.tabpanel')
+const isEmpty = (str) => !str.trim().length
+let currentStep = 0
+
+//loading input field data
+let page1data1 = document.querySelector("#page1data1")
+let page1show1 = document.querySelector("#page1show1")
+let page2data1 = document.querySelector("#page2data1")
+let page2show1 = document.querySelector("#page2show1")
+let page3data1 = document.querySelector("#page3data1")
+let page3show1 = document.querySelector("#page3show1")
+
+
+
+// Validate first input on load
+validateEntry()
+
+// Next: Change UI relative to current step and account for button permissions
+nextButton.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  console.log("next Button Clicked")
+  // Hide current tab
+  tabPanels[currentStep].classList.add('hidden')
+  tabTargets[currentStep].classList.remove('active')
+
+  // Show next tab
+  tabPanels[currentStep + 1].classList.remove('hidden')
+  tabTargets[currentStep + 1].classList.add('active')
+  currentStep += 1
+  
+  validateEntry()
+  updateStatusDisplay()
+})
+
+// Previous: Change UI relative to current step and account for button permissions
+previousButton.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log("previous Button Clicked")
+
+  // Hide current tab
+  tabPanels[currentStep].classList.add('hidden')
+  tabTargets[currentStep].classList.remove('active')
+
+  // Show previous tab
+  tabPanels[currentStep - 1].classList.remove('hidden')
+  tabTargets[currentStep - 1].classList.add('active')
+  currentStep -= 1
+
+  nextButton.removeAttribute('disabled')
+  updateStatusDisplay()
+})
+
+//submit button
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault
+  console.log("form submitted")
+  alert("form submitted")
+  document.write("form submitted")
+  let output = {
+    "step1": page1data1.value,
+    "step2": page2data1.value,
+    "step3": page3data1.value
+  }
+  console.log(output);
+
+})
+
+function updateStatusDisplay() {
+  // If on the last step, hide the next button and show submit
+  if (currentStep === tabTargets.length - 1) {
+    nextButton.classList.add('hidden')
+    previousButton.classList.remove('hidden')
+    submitButton.classList.remove('hidden')
+    validateEntry()
+
+    // If it's the first step hide the previous button
+  } else if (currentStep == 0) {
+    nextButton.classList.remove('hidden')
+    previousButton.classList.add('hidden')
+    submitButton.classList.add('hidden')
+    // In all other instances display both buttons
+  } else {
+    nextButton.classList.remove('hidden')
+    previousButton.classList.remove('hidden')
+    submitButton.classList.add('hidden')
+  }
+}
+
+function validateEntry() {
+  let input = tabPanels[currentStep].querySelector('.form-input')
+  
+  // Start but disabling continue button
+  nextButton.setAttribute('disabled', true)
+  submitButton.setAttribute('disabled', true)
+  
+  // Validate on initial function fire
+  setButtonPermissions(input)
+  
+  // Validate on input
+  input.addEventListener('input', () => setButtonPermissions(input))
+  // Validate if bluring from input
+  input.addEventListener('blur', () => setButtonPermissions(input))
+}
+
+function setButtonPermissions(input) {
+  if (isEmpty(input.value)) {
+    nextButton.setAttribute('disabled', true)
+    submitButton.setAttribute('disabled', true)
+  } else {
+    nextButton.removeAttribute('disabled')
+    submitButton.removeAttribute('disabled')
+  }
+}
+
+console.log("Script.js loaded")
